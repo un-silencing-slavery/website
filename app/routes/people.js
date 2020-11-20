1664e54b197cbc0eaea473228712f7c3304d8740
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-11-20 15:31:09 -0500

    feat: Implement fake data on PeopleRoute

--- app/routes/people.js
+++ app/routes/people.js
@@ -4,15 +4,29 @@ import faker from "faker";
 export default class PeopleRoute extends Route {
   model() {
     const people = [];
+
     for(let i = 1; i < 209; i += 1){
+      const birthYear = Math.floor(Math.random() * 70) + 1760;
+      const exitYear = Math.random() < 0.66 
+        ? 1832 
+        : Math.floor(Math.random() * 13) + 1818;
+      const name = faker.name.firstName();
       const person = this.store.createRecord("person", {
-        name: faker.name.firstName(),
+        id: `p${i}`,
+        name,
         origin: "Creole" , 
-        birthYear: 1800 ,
-        exitYear: 1832 ,
-        slug: "blah-1" ,
-        gender: "Female" ,
-        color: "Quadroon",
+        birthYear,
+        exitYear,
+        slug: `${name.toLowerCase()}-p${i}`,
+        gender: Math.random() < 0.5 ? "Male" : "Female",
+        color: Math.random() < 0.85 
+        ? "Negro" : Math.random() < 0.33 
+        ? "Quadroon" : Math.random() < 0.5
+        ? "Sambo" : "Mulatto",
+        origin: Math.random() < 0.72
+        ? "Creole" : Math.random() < 0.90 
+        ? "African" : Math.random() < 0.5
+        ? "Creole/African?" : "not_specified",
       });
 
       people.push(person);
