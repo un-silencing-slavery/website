02489561a0b7a1204b12bd44070ecdd9a0de0adc
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-11-20 16:43:45 -0500

    feat: Remove model from people route

--- app/routes/people.js
+++ app/routes/people.js
@@ -2,36 +2,4 @@ import Route from '@ember/routing/route';
 import faker from "faker";
 
 export default class PeopleRoute extends Route {
-  model() {
-    const people = [];
-
-    for(let i = 1; i < 209; i += 1){
-      const birthYear = Math.floor(Math.random() * 70) + 1760;
-      const exitYear = Math.random() < 0.66 
-        ? 1832 
-        : Math.floor(Math.random() * 13) + 1818;
-      const name = faker.name.firstName();
-      const person = this.store.createRecord("person", {
-        id: `p${i}`,
-        name,
-        origin: "Creole" , 
-        birthYear,
-        exitYear,
-        slug: `${name.toLowerCase()}-p${i}`,
-        gender: Math.random() < 0.5 ? "Male" : "Female",
-        color: Math.random() < 0.85 
-        ? "Negro" : Math.random() < 0.33 
-        ? "Quadroon" : Math.random() < 0.5
-        ? "Sambo" : "Mulatto",
-        origin: Math.random() < 0.72
-        ? "Creole" : Math.random() < 0.90 
-        ? "African" : Math.random() < 0.5
-        ? "Creole/African?" : "not_specified",
-      });
-
-      people.push(person);
-    }
-
-    return people;
-  }
 }
