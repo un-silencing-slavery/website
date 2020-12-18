3fe3c52aa0254753104307008f98b23addb2046f
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-12-18 16:40:47 -0500

    feat: Add scaled years to wedges

--- app/components/community-tree.js
+++ app/components/community-tree.js
@@ -1,4 +1,5 @@
 import Component from '@glimmer/component';
+import { scaleLinear } from "d3-scale";
 
 export default class CommunityTreeComponent extends Component {
   width = 250;
@@ -6,6 +7,12 @@ export default class CommunityTreeComponent extends Component {
   height = 250;
 
   dataLength = this.args.data.length;
+
+  get yearScale() {
+    return scaleLinear()
+      .domain([1817, 1832])
+      .range([0, this.height / 2]);
+  }
 
   // transform(i) {
   //   const string = `translate(25,${i * 10})`;
--- app/components/community-tree/person.js
+++ app/components/community-tree/person.js
@@ -6,10 +6,20 @@ export default class CommunityTreePersonComponent extends Component {
 
   get arc(){
     return arc()
-    .innerRadius((this.args.i + 1) * 10)
-    .outerRadius(100)
+    .innerRadius(this.scaledArrivalYear)
+    .outerRadius(this.args.yearScale(this.args.person.exitYear))
     .padAngle(Math.PI / 270)
     .startAngle(this.args.i * 2 * Math.PI / this.args.dataLength)
     .endAngle((this.args.i + 1) * 2 * Math.PI / this.args.dataLength)();
   }
+
+  get scaledArrivalYear(){
+    let year = 1817;
+    if (this.args.person.birthYear > 1817) {
+      year = this.args.person.birthYear;
+    }
+
+    return this.args.yearScale(year)
+  }
+
 }
