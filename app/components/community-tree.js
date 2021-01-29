e96c46d5c1547115dd2af5e9b7efa379b9c26061
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-01-29 16:56:27 -0500

    feat: Incorporate broken radial gradients in Person wedges

--- app/components/community-tree.js
+++ app/components/community-tree.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { scaleLinear } from "d3-scale";
+import { interpolateCool } from "d3-scale-chromatic";
 
 export default class CommunityTreeComponent extends Component {
   width = 400;
@@ -7,6 +8,20 @@ export default class CommunityTreeComponent extends Component {
   height = 400;
 
   dataLength = this.args.data.length;
+
+  get beginningStop(){
+    return `stop-color: ${interpolateCool(0)}; `;
+  }
+
+  get endingStop(){
+    return `stop-color: ${interpolateCool(1)}; `;
+  }
+
+  get colorScale() {
+    return scaleLinear()
+      .domain([0,15])
+      .range([0,1]);
+  }
 
   get yearScale() {
     return scaleLinear()
