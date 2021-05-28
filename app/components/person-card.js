240679d04be326ded38c51afaa3ca063a56c6461
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-05-28 16:50:18 -0400

    Add some styling and improve animation of person cards.

--- app/components/person-card.js
+++ app/components/person-card.js
@@ -2,12 +2,26 @@ import Component from '@glimmer/component';
 import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
+import { fadeOut, fadeIn } from 'ember-animated/motions/opacity';
+// import { scale } from 'ember-animated/motions/scale';
 
 export default class PersonCardComponent extends Component {
 
   @service activePerson;
 
   @tracked hiddenBio = true;
+
+  @action *transition({ insertedSprites, removedSprites }) {
+    insertedSprites.forEach(sprite => {
+      fadeIn(sprite);
+      // scale(sprite);
+    });
+    removedSprites.forEach(sprite => {
+      fadeOut(sprite);
+      // scale(sprite);
+    });
+  }
+
 
   @action toggleBiography() {
     this.hiddenBio = !this.hiddenBio;
