ef989c6822edab951e63c02202d1f729fd99b157
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-09-30 17:07:50 -0400

    Rebasing?

---
+++ app/components/squiggly-line/circle.js
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+import { action } from "@ember/object";
+
+export default class SquigglyLineCircleComponent extends Component {
+  @action innerCircleHandleClick() {
+  }
+
+}
