69e4cae5afafcaf8a6635157d5cb76ea276ddeb3
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-03-27 16:01:59 -0400

    Improve animation on person card movement and style expanded card

--- app/components/persons-list.js
+++ app/components/persons-list.js
@@ -1,4 +1,4 @@
-import Component from '@glimmer/component';
+import Component from "@glimmer/component";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import move from "ember-animated/motions/move";
@@ -8,24 +8,34 @@ export default class PersonsListComponent extends Component {
 
   @action
   *transition({ keptSprites }) {
-
-    keptSprites.forEach(move);
+    keptSprites.forEach((sprite) => {
+      if (sprite.element.dataset.personId === this.activePerson.personId) {
+        sprite.applyStyles({
+          "z-index": "100",
+        });
+      } else {
+        sprite.applyStyles({
+          "z-index": "0",
+        });
+      }
+      move(sprite);
+    });
   }
 
   get data() {
     const model = this.args.model.toArray();
-    if(this.activePerson.personId){
+    if (this.activePerson.personId) {
       const personIdsArray = this.args.model.mapBy("id");
       const data = [];
       const activeIndex = personIdsArray.indexOf(this.activePerson.personId);
       data.push(model[activeIndex]);
-      if(activeIndex > 0){
-        for(const person of model.slice(0,activeIndex)){
+      if (activeIndex > 0) {
+        for (const person of model.slice(0, activeIndex)) {
           data.push(person);
         }
       }
-      if (activeIndex < model.length - 1){
-        for(const person of model.slice(activeIndex + 1, model.length - 1)){
+      if (activeIndex < model.length - 1) {
+        for (const person of model.slice(activeIndex + 1, model.length - 1)) {
           data.push(person);
         }
       }
