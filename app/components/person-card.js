7585bf11027210910356e026b70a76a31842c46c
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-03-27 16:01:59 -0400

    Improve animation on person card movement and style expanded card

--- app/components/person-card.js
+++ app/components/person-card.js
@@ -1,30 +1,32 @@
-import Component from '@glimmer/component';
+import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
-import { fadeOut, fadeIn } from 'ember-animated/motions/opacity';
+import { fadeOut, fadeIn } from "ember-animated/motions/opacity";
 // import { scale } from 'ember-animated/motions/scale';
 
 export default class PersonCardComponent extends Component {
-
   @service activePerson;
+
+  get active() {
+    return this.activePerson.personId === this.args.person.id;
+  }
 
   @tracked hiddenBio = true;
 
   @action *transition({ insertedSprites, removedSprites }) {
-    insertedSprites.forEach(sprite => {
+    insertedSprites.forEach((sprite) => {
       fadeIn(sprite);
       // scale(sprite);
     });
-    removedSprites.forEach(sprite => {
+    removedSprites.forEach((sprite) => {
       fadeOut(sprite);
       // scale(sprite);
     });
   }
 
-
   @action
-  setActivePerson(){
+  setActivePerson() {
     this.activePerson.personId = this.args.person.id;
   }
 }
