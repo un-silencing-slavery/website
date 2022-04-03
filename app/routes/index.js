988bfa655d56a34e01419ae277a41551494e7d29
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-03 16:27:53 -0400

    Move data over to mirage.

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,48 +5,49 @@ import faker from "faker";
 export default class IndexRoute extends Route {
   @service store;
 
-  model() {
-    const people = [];
+  async model() {
+    // const people = [];
 
-    for (let i = 1; i < 208; i += 1) {
-      const birthYear = Math.floor(Math.random() * 70) + 1760;
-      let exitYear =
-        Math.random() < 0.66
-          ? 1832
-          : birthYear < 1818
-          ? Math.floor(Math.random() * 13) + 1818
-          : Math.floor(Math.random() * (1832 - birthYear) + birthYear);
-      const name = faker.name.firstName();
-      const person = this.store.createRecord("person", {
-        id: `p${i}`,
-        name,
-        birthYear,
-        exitYear: (exitYear += 0.1),
-        slug: `${name.toLowerCase()}-p${i}`,
-        gender: Math.random() < 0.5 ? "Male" : "Female",
-        color:
-          Math.random() < 0.85
-            ? "Negro"
-            : Math.random() < 0.33
-            ? "Quadroon"
-            : Math.random() < 0.5
-            ? "Sambo"
-            : "Mulatto",
-        origin:
-          Math.random() < 0.72
-            ? "Creole"
-            : Math.random() < 0.9
-            ? "African"
-            : Math.random() < 0.5
-            ? "Creole/African?"
-            : "not_specified",
-      });
+    // for (let i = 1; i < 208; i += 1) {
+    //   const birthYear = Math.floor(Math.random() * 70) + 1760;
+    //   let exitYear =
+    //     Math.random() < 0.66
+    //       ? 1832
+    //       : birthYear < 1818
+    //       ? Math.floor(Math.random() * 13) + 1818
+    //       : Math.floor(Math.random() * (1832 - birthYear) + birthYear);
+    //   const name = faker.name.firstName();
+    //   const person = this.store.createRecord("person", {
+    //     id: `p${i}`,
+    //     name,
+    //     birthYear,
+    //     exitYear: (exitYear += 0.1),
+    //     slug: `${name.toLowerCase()}-p${i}`,
+    //     gender: Math.random() < 0.5 ? "Male" : "Female",
+    //     color:
+    //       Math.random() < 0.85
+    //         ? "Negro"
+    //         : Math.random() < 0.33
+    //         ? "Quadroon"
+    //         : Math.random() < 0.5
+    //         ? "Sambo"
+    //         : "Mulatto",
+    //     origin:
+    //       Math.random() < 0.72
+    //         ? "Creole"
+    //         : Math.random() < 0.9
+    //         ? "African"
+    //         : Math.random() < 0.5
+    //         ? "Creole/African?"
+    //         : "not_specified",
+    //   });
 
-      people.push(person);
-    }
+    //   people.push(person);
+    // }
 
-    // return people;
-    return people.sort((a, b) => a.birthYear - b.birthYear); //.filter(p => p.birthYear > 1810);
-    // return [people[0]];
+    // // return people;
+    // return people.sort((a, b) => a.birthYear - b.birthYear); //.filter(p => p.birthYear > 1810);
+    // // return [people[0]];
+    return this.store.findAll("person");
   }
 }
