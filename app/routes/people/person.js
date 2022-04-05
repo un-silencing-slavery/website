316cc5d7327bee61248f93b65641f3b0b1c5a898
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-05 10:50:24 -0400

    Inject data in Main component, not on the route.

--- app/routes/people/person.js
+++ app/routes/people/person.js
@@ -1,11 +1,7 @@
 import Route from "@ember/routing/route";
 
 export default class PeoplePersonRoute extends Route {
-  async model({ personId }) {
-    const people = await this.modelFor("index");
-    return {
-      personId,
-      people,
-    };
+  model({ personId }) {
+    return personId;
   }
 }
