2df3981ea05a576c4fe1e8313fb09ac82cf2df54
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-06 14:07:19 -0400

    Keep trying to do the family sort.

--- app/services/data.ts
+++ app/services/data.ts
@@ -2,6 +2,12 @@ import Service from "@ember/service";
 import { tracked } from "@glimmer/tracking";
 import people from "rose-hall/data/people";
 
+type PersonId = string | null;
+interface Family {
+  mother: PersonId;
+  children: (PersonId | Family)[];
+}
+
 export default class DataService extends Service {
   @tracked people = people as Person[];
 
@@ -15,56 +21,24 @@ export default class DataService extends Service {
         this.people.sort((a, b) => a.name.localeCompare(b.name));
         break;
       case "age":
-        this.people.sort((a, b) => {
-          let aBirthYear = 1833;
-          let bBirthYear = 1833;
-          if (a.birthYear) {
-            aBirthYear = a.birthYear;
-          }
-          if (b.birthYear) {
-            bBirthYear = b.birthYear;
-          }
-          return aBirthYear - bBirthYear;
-        });
+        this.people.sort(
+          (a, b) => (a.birthYear || 1833) - (b.birthYear || 1833)
+        );
         break;
       case "race":
-        this.people.sort((a, b) => {
-          let aColor = "unknown";
-          let bColor = "unknown";
-          if (a.color) {
-            aColor = a.color;
-          }
-          if (b.color) {
-            bColor = b.color;
-          }
-          return aColor.localeCompare(bColor);
-        });
+        this.people.sort((a, b) =>
+          (a.color || "z").localeCompare(b.color || "z")
+        );
         break;
       case "origin":
-        this.people.sort((a, b) => {
-          let aCountry = "unknown";
-          let bCountry = "unknown";
-          if (a.country) {
-            aCountry = a.country;
-          }
-          if (b.country) {
-            bCountry = b.country;
-          }
-          return aCountry.localeCompare(bCountry);
-        });
+        this.people.sort((a, b) =>
+          (a.country || "z").localeCompare(b.country || "z")
+        );
         break;
       case "gender":
-        this.people.sort((a, b) => {
-          let aGender = "unknown";
-          let bGender = "unknown";
-          if (a.gender) {
-            aGender = a.gender;
-          }
-          if (b.gender) {
-            bGender = b.gender;
-          }
-          return aGender.localeCompare(bGender);
-        });
+        this.people.sort((a, b) =>
+          (a.gender || "z").localeCompare(b.gender || "z")
+        );
         break;
       case "family":
         // const people = [];
@@ -78,19 +52,9 @@ export default class DataService extends Service {
   }
 
   sortByFamily() {
-    const people = []; //: Person[] = [];
-    const greatGrandmotherIds = [
-      ...new Set(this.people.map((person) => person.greatgrandmotherId)),
-    ];
-    const grandmotherIds = [
-      ...new Set(this.people.map((person) => person.grandmotherId)),
-    ];
-    const motherIds = [
-      ...new Set(this.people.map((person) => person.motherId)),
-    ];
-
-    console.log(greatGrandmotherIds, grandmotherIds, motherIds);
-    // return this.people;
+    this.people.sort((a, b) =>
+      (a.motherId || "unknown").localeCompare(b.motherId || "unknown")
+    );
   }
 }
 
