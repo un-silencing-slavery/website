5974b59d21e9ad5392b86636beac6f7d61a9c71e
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-06 23:04:00 -0400

    Keep trying stuff for family sort.

--- app/services/data.ts
+++ app/services/data.ts
@@ -46,33 +46,88 @@ export default class DataService extends Service {
   }
 
   sortByFamily() {
-    const moveItem = (fromIndex: number, toIndex: number) => {
-      const element = this.people[fromIndex];
-      this.people.splice(fromIndex, 1);
-      this.people.splice(toIndex, 0, element);
-    };
+    this.people.sort((a, b) => {
+      if (a.personId === b.motherId) {
+        return -1;
+      } else if (b.personId === a.motherId) {
+        return 1;
+      }
 
-    this.people.sort((a, b) =>
-      (a.motherId || "unknown").localeCompare(b.motherId || "unknown")
-    );
+      return 0;
+    });
 
-    let index = this.people.length - 1;
-    while (index >= 0) {
-      const person = this.people[index];
+    // const moveItem = (fromIndex: number, toIndex: number) => {
+    //   const element = this.people[fromIndex];
+    //   this.people.splice(fromIndex, 1);
+    //   this.people.splice(toIndex, 0, element);
+    // };
+
+    /*
+    const generationsOfDescendents = (person: Person) => {
+      let generations = 0;
+      let descendants = 0;
       const children = this.people.filter(
         (child) => child.motherId === person.personId
       );
       if (children.length > 0) {
-        const firstChildIndex = this.people.indexOf(children[0]);
-        if (index !== firstChildIndex - 1) {
-          moveItem(index, firstChildIndex);
-        } else {
-          index -= 1;
+        generations += 1;
+        descendants += children.length;
+        const grandchildren = this.people.filter(
+          (grandchild) =>
+            grandchild.motherId &&
+            children
+              .map((child) => child.personId)
+              .includes(grandchild.motherId)
+        );
+        if (grandchildren.length > 0) {
+          generations += 1;
+          descendants += grandchildren.length;
+          const greatgrandchildren = this.people.filter(
+            (greatgrandchild) =>
+              greatgrandchild.motherId &&
+              grandchildren
+                .map((grandchild) => grandchild.personId)
+                .includes(greatgrandchild.motherId)
+          );
+          if (greatgrandchildren.length > 0) {
+            generations += 1;
+            descendants += greatgrandchildren.length;
+          }
         }
-      } else {
-        index -= 1;
       }
-    }
+  
+      return { generations, descendants };
+    };
+  
+    this.people.sort((a, b) => {
+      const aFamily = generationsOfDescendents(a);
+      const bFamily = generationsOfDescendents(b);
+  
+      if (aFamily.generations === bFamily.generations) {
+        return bFamily.descendants - aFamily.descendants;
+      }
+  
+      return bFamily.generations - aFamily.generations;
+    });
+    */
+
+    // let index = this.people.length - 1;
+    // while (index >= 0) {
+    //   const person = this.people[index];
+    //   const children = this.people.filter(
+    //     (child) => child.motherId === person.personId
+    //   );
+    //   if (children.length > 0) {
+    //     const firstChildIndex = this.people.indexOf(children[0]);
+    //     if (index !== firstChildIndex - 1) {
+    //       moveItem(index, firstChildIndex);
+    //     } else {
+    //       index -= 1;
+    //     }
+    //   } else {
+    //     index -= 1;
+    //   }
+    // }
   }
 }
 
