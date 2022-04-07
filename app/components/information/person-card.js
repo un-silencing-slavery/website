beef674d590cd4a7d54595229d238713eac71aa2
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-06 23:04:00 -0400

    Keep trying stuff for family sort.

--- app/components/information/person-card.js
+++ app/components/information/person-card.js
@@ -1,13 +1,61 @@
-/* eslint require-yield: "off" */
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 import { service } from "@ember/service";
-import { fadeOut, fadeIn } from "ember-animated/motions/opacity";
-// import { scale } from 'ember-animated/motions/scale';
 
 export default class InformationPersonCardComponent extends Component {
   @service activePerson;
 
   @service data;
+
+  @tracked childrenNumber = "no children. ";
+
+  get children() {
+    const children = this.data.people.filter(
+      (person) => person.motherId === this.person.personId
+    );
+    if (children.length < 1) {
+      return false;
+    } else {
+      const names = children.map((child) => child.name);
+      if (names.length === 1) {
+        this.childrenNumber = "one child, ";
+        return names[0];
+      } else if (names.length === 2) {
+        this.childrenNumber = `a pair of children, `;
+        return names.map((name) => name).join(" and ");
+      } else {
+        this.childrenNumber = `${names.length} children, `;
+        names[names.length - 1] = `and ${names[names.length - 1]}`;
+        return names.map((name) => name.trim()).join(", ");
+      }
+    }
+  }
+
+  get mother() {
+    const mother = this.person.motherId;
+    if (mother) {
+      return this.data.people.filter((person) => person.personId === mother)[0]
+        .name;
+    }
+
+    return false;
+  }
+
+  get color() {
+    return this.person.color || false;
+  }
+
+  get origin() {
+    return this.person.country || false;
+  }
+
+  get birthYear() {
+    return this.person.birthYear || false;
+  }
+
+  get exitYear() {
+    return this.person.exitYear || false;
+  }
 
   get person() {
     return this.data.people.filter(
--- app/components/information/persons-list-item.js
+++ app/components/information/persons-list-item.js
@@ -1,9 +1,55 @@
 import Component from "@glimmer/component";
 import { service } from "@ember/service";
 import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
 
 export default class InformationPersonsListItemComponent extends Component {
   @service activePerson;
+
+  @service data;
+
+  get familyIndent() {
+    let indent = { class: "", text: "" };
+    if (this.data.sortKey !== "family") {
+      this.isMother = false;
+      return "";
+    }
+
+    const children = this.data.people.filter(
+      (person) => person.motherId === this.args.person.personId
+    );
+    if (children.length > 0) {
+      this.isMother = true;
+    }
+
+    if (this.args.person.motherId) {
+      const mother = this.data.people.filter(
+        (person) => person.personId === this.args.person.motherId
+      )[0];
+      indent.text = "└ ";
+      indent.class = "pl-1";
+      if (mother.motherId) {
+        const grandmothers = this.data.people.filter(
+          (person) => person.personId === mother.motherId
+        );
+        indent.class = "pl-3";
+
+        if (grandmothers.length > 0) {
+          const grandmother = grandmothers[0];
+          const greatgrandmothers = this.data.people.filter(
+            (person) => person.personId === grandmother.motherId
+          );
+          if (greatgrandmothers.length > 0) {
+            indent.class = "pl-5";
+          }
+        }
+      }
+    }
+
+    return indent;
+  }
+
+  @tracked isMother = false;
 
   get active() {
     return this.activePerson.personId === this.args.person.personId;
