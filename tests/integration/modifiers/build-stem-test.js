56171e54c5836fec2d22311e9904c49f46f8efb4
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-10 01:47:50 -0400

    Get working leaves.

--- tests/integration/modifiers/build-stem-test.js
+++ tests/integration/modifiers/build-stem-test.js
@@ -3,12 +3,12 @@ import { setupRenderingTest } from "ember-qunit";
 import { render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 
-module("Integration | Modifier | build-frond", function (hooks) {
+module("Integration | Modifier | build-stem", function (hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
   test("it renders", async function (assert) {
-    await render(hbs`<div {{build-frond}}></div>`);
+    await render(hbs`<div {{build-stem}}></div>`);
 
     assert.ok(true);
   });
