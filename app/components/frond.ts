31e52c01b10808e5a329f8c57df2978051b6e9a6
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-09 20:33:40 -0400

    feat: start working with a modifier to build fronds.

--- app/components/frond.ts
+++ app/components/frond.ts
@@ -1,48 +1,21 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import { action } from "@ember/object";
+// import { action } from "@ember/object";
 import SvgService from "rose-hall/services/svg";
-import { path, curveCatmullRom } from "d3";
 import { service } from "@ember/service";
 
 export default class FrondComponent extends Component {
   @service declare svg: SvgService;
 
-  @tracked stem: SVGPathElement | null = null;
+  @tracked declare stem: SVGPathElement;
 
-  @action setStem(path: SVGPathElement) {
-    this.stem = path;
-    // this.dots = [1, 2, 3].map((i) => {
-    //   return path.getPointAtLength(i * 0.25 * path.getTotalLength());
-    // });
-  }
+  // get dots() {
+  //   return [
+  //     { x: 200, y: 200 },
+  //     { x: 300, y: 300 },
+  //     { x: 400, y: 400 },
+  //   ];
+  // }
 
-  get path() {
-    const d = path();
-    const curve = curveCatmullRom(d);
-    const line = [
-      [0.1 * this.svg.width, 0.5 * this.svg.height],
-      [0.9 * this.svg.width, 0.5 * this.svg.height],
-    ];
-    curve.lineStart();
-    for (const [x, y] of line) curve.point(x, y);
-    curve.lineEnd();
-
-    return d;
-  }
-
-  get dots() {
-    console.log("in get dots");
-    if (this.stem instanceof SVGPathElement) {
-      return [
-        this.stem.getPointAtLength(this.stem.getTotalLength() / 4),
-        this.stem.getPointAtLength(this.stem.getTotalLength() / 2),
-        this.stem.getPointAtLength(this.stem.getTotalLength() / 1.333),
-      ];
-    }
-
-    return [];
-  }
-
-  // @tracked declare dots: SVGPoint[];
+  @tracked declare leaves: SVGPoint[];
 }
