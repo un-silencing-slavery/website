846edbe2f15875e9a37131c824281070e4fd1894
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-09 23:57:03 -0400

    Get something close to a basic frond working.

--- app/components/frond.ts
+++ app/components/frond.ts
@@ -1,21 +1,37 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-// import { action } from "@ember/object";
+import { action } from "@ember/object";
 import SvgService from "rose-hall/services/svg";
 import { service } from "@ember/service";
 
 export default class FrondComponent extends Component {
   @service declare svg: SvgService;
 
+  @action pathResize() {
+    console.log(this.svg.width);
+  }
+
+  pointAtLength(segmentPct: number) {
+    if (this.svg.frond) {
+      return this.svg.frond.getPointAtLength(
+        segmentPct * this.svg.frond.getTotalLength()
+      );
+    }
+
+    return { x: 0, y: 0 };
+  }
+
   @tracked declare stem: SVGPathElement;
 
-  // get dots() {
-  //   return [
-  //     { x: 200, y: 200 },
-  //     { x: 300, y: 300 },
-  //     { x: 400, y: 400 },
-  //   ];
-  // }
+  get leaves() {
+    if (this.svg.frond) {
+      return [1, 2, 3].map((i) => this.pointAtLength(i * 0.25));
+    }
 
-  @tracked declare leaves: SVGPoint[];
+    return [
+      { x: 200, y: 200 },
+      { x: 300, y: 300 },
+      { x: 400, y: 400 },
+    ];
+  }
 }
