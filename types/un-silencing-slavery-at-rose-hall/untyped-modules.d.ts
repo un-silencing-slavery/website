b12dee921b34239a44e92b720bbe6ebc0592ff0e
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-16 16:45:35 -0400

    feat: Create nav menu and header for mobile.

--- types/un-silencing-slavery-at-rose-hall/untyped-modules.d.ts
+++ types/un-silencing-slavery-at-rose-hall/untyped-modules.d.ts
@@ -1,5 +1,12 @@
 declare module "ember-promise-modals" {
+  import ModalsIndexComponent from "un-silencing-slavery-at-rose-hall/components/modals-index";
+
+  interface OpenModalOptions {
+    close(component: ModalsIndexComponent): void;
+  }
+
   export default interface Modals {
-    open: (content: string) => void;
+    open(component: ModalsIndexComponent, options: OpenModalOptions): void;
+    close(component: ModalsIndexComponent): void;
   }
 }
