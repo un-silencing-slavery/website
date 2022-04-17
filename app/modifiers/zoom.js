ad504696faa154f2e2192b44514afc22a4579e53
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-04-16 20:30:35 -0400

    Implement zoom.

---
+++ app/modifiers/zoom.js
@@ -0,0 +1,24 @@
+import Modifier from "ember-modifier";
+import { service } from "@ember/service";
+import { select, zoom } from "d3";
+
+export default class ZoomModifier extends Modifier {
+  @service svg;
+
+  modify(element) {
+    select("svg.community-tree").call(
+      zoom()
+        .extent([
+          [0, 0],
+          [this.svg.width, this.svg.height],
+        ])
+        .scaleExtent([1, 8])
+        .on("zoom", this.zoomed)
+    );
+  }
+
+  zoomed({ transform }) {
+    console.log(transform);
+    select("g.zoomable").attr("transform", transform);
+  }
+}
