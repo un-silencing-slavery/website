cdaac4cf4a191538de4ef6d866ecd044c5d94fd6
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-07-23 15:55:13 -0400

    feat: add 'Person' to person title.

--- app/routes/index/person.js
+++ app/routes/index/person.js
@@ -6,19 +6,19 @@ export default class IndexPersonRoute extends Route {
 
   @service data;
 
+  afterModel(model) {
+    super.afterModel(...arguments);
+
+    this.metaInfo = {
+      title: `${model.name} | People | (Un)Silencing Slavery`,
+    };
+  }
+
   model({ person_id }) {
     this.activePerson.setActivePerson(person_id);
     return this.data.people.filter(
       (person) => person.personId === person_id
     )[0];
-  }
-
-  afterModel(model) {
-    super.afterModel(...arguments);
-
-    this.metaInfo = {
-      title: `${model.name} | (Un)Silencing Slavery`,
-    };
   }
 
   willTransition() {
