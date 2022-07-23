6fce9601cd607093718d0d43e142ffb6ebcbecc9
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-07-23 13:43:06 -0400

    feat: Add page-title modifier

--- app/components/information/person-card.js
+++ app/components/information/person-card.js
@@ -1,5 +1,4 @@
 import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
 import { service } from "@ember/service";
 import { action } from "@ember/object";
 
@@ -8,77 +7,8 @@ export default class InformationPersonCardComponent extends Component {
 
   @service activePerson;
 
-  @service data;
-
-  @tracked childrenNumber = "no children. ";
-
   @action goBack() {
     this.activePerson.clearActivePerson();
     this.router.transitionTo("index");
-  }
-
-  get children() {
-    const children = this.data.people.filter(
-      (person) => person.motherId === this.person.personId
-    );
-    if (children.length < 1) {
-      return false;
-    } else {
-      const names = children.map((child) => child.name);
-      if (names.length === 1) {
-        this.setChildrenNumber("one child, ");
-        return names[0];
-      } else if (names.length === 2) {
-        this.setChildrenNumber(`a pair of children, `);
-        return names.map((name) => name).join(" and ");
-      } else {
-        this.setChildrenNumber(`${names.length} children, `);
-        names[names.length - 1] = `and ${names[names.length - 1]}`;
-        return names.map((name) => name.trim()).join(", ");
-      }
-    }
-  }
-
-  setChildrenNumber(string) {
-    this.childrenNumber = string;
-  }
-
-  get mother() {
-    const mother = this.person.motherId;
-    if (mother) {
-      return this.data.people.filter((person) => person.personId === mother)[0]
-        .name;
-    }
-
-    return false;
-  }
-
-  get color() {
-    return this.person.color || false;
-  }
-
-  get origin() {
-    return this.person.country || false;
-  }
-
-  get birthYear() {
-    return this.person.birthYear || false;
-  }
-
-  get exitYear() {
-    return this.person.exitYear || false;
-  }
-
-  get person() {
-    return this.data.people.filter(
-      (person) => person.personId === this.activePerson.personId
-    )[0];
-  }
-
-  constructor() {
-    super(...arguments);
-    // if (!this.activePerson.personId) {
-    //   this.activePerson.setActivePerson(this.args.personId);
-    // }
   }
 }
--- app/routes/index/person.js
+++ app/routes/index/person.js
@@ -4,12 +4,16 @@ import { service } from "@ember/service";
 export default class IndexPersonRoute extends Route {
   @service activePerson;
 
+  @service data;
+
   model({ person_id }) {
     this.activePerson.setActivePerson(person_id);
+    return this.data.people.filter(
+      (person) => person.personId === person_id
+    )[0];
   }
 
   willTransition() {
-    console.log("nulling out activeperson");
     this.activePerson.clearActivePerson();
   }
 }
