43452fc8388ee19cc75b8a28cc16e745b5cfc7c4
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-08-01 17:56:08 -0400

    feat: Clean up sort button/menu.

--- app/components/information/header.ts
+++ app/components/information/header.ts
@@ -2,10 +2,13 @@ import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 import { service } from "@ember/service";
+import DataService from "un-silencing-slavery/services/data";
 import RouterService from "@ember/routing/router-service";
 
 export default class InformationHeaderComponent extends Component {
   @tracked sortmenuVisible = false;
+
+  @service declare data: DataService;
 
   @service declare router: RouterService;
 
