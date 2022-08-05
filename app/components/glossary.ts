48ee92e6d37c215fc1ee67f2aa0c302dfff0ab1c
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-08-05 00:20:58 -0400

    feat: Implement opacity css-transition

--- app/components/glossary.ts
+++
@@ -1,8 +0,0 @@
-import Component from "@glimmer/component";
-import { service } from "@ember/service";
-import AnnotationsService from "un-silencing-slavery/services/annotations";
-import { htmlSafe } from "@ember/template";
-
-export default class GlossaryComponent extends Component {
-  @service declare annotations: AnnotationsService;
-}
