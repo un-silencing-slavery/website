107c276a92425003353e14b37e40a7015c7e890c
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-08-05 02:11:23 -0400

    feat: Clean up some loose ends.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,23 +1,4 @@
 import Route from "@ember/routing/route";
 import "un-silencing-slavery/style/styles.css";
 
-export default class ApplicationRoute extends Route {
-  headTags = [
-    {
-      type: "meta",
-      tagId: "meta-og-name",
-      attrs: {
-        property: "og:name",
-        content: "(Un)Silencing Slavery",
-      },
-    },
-    {
-      type: "link",
-      tagId: "canonical-link",
-      attrs: {
-        rel: "canonical",
-        content: "https://rose-hall.moacir.com",
-      },
-    },
-  ];
-}
+export default class ApplicationRoute extends Route { }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,8 @@
 import Route from "@ember/routing/route";
 
 export default class IndexRoute extends Route {
-  headTags = [
-    {
-      type: "meta",
-      tagId: "meta-og-name",
-      attrs: {
-        property: "og:name",
-        content: "boo (Un)Silencing Slavery",
-      },
-    },
-    {
-      type: "link",
-      tagId: "canonical-link",
-      attrs: {
-        rel: "canonical",
-        content: "https://barrose-hall.moacir.com",
-      },
-    },
-  ];
+  metaInfo = {
+    canonical: "https://rosehallproject.columbia.edu",
+    date: "2022-08-06",
+  };
 }
