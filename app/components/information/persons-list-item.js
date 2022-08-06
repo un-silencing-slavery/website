1e60870833b3b8e5bf470d42d89433eeb34d4223
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-08-06 15:24:23 -0400

    feat: get matrilineage working fully in the persons list.

--- app/components/information/persons-list-item.js
+++ app/components/information/persons-list-item.js
@@ -8,59 +8,12 @@ export default class InformationPersonsListItemComponent extends Component {
 
   @service data;
 
-  setIsMother(newValue) {
-    this.isMother = newValue;
-  }
-
-  get familyIndent() {
-    let indent = { class: "", text: "" };
-    if (this.data.sortKey !== "matrilineage") {
-      this.setIsMother(false);
-      return "";
-    }
-
-    const children = this.data.people.filter(
-      (person) => person.motherId === this.args.person.personId
-    );
-    if (children.length > 0) {
-      this.setIsMother(true);
-    }
-
-    if (this.args.person.motherId) {
-      const mother = this.data.people.filter(
-        (person) => person.personId === this.args.person.motherId
-      )[0];
-      indent.text = "└";
-      indent.class = "pl-2";
-      if (mother.motherId) {
-        const grandmothers = this.data.people.filter(
-          (person) => person.personId === mother.motherId
-        );
-        indent.class = "pl-4";
-
-        if (grandmothers.length > 0) {
-          const grandmother = grandmothers[0];
-          const greatgrandmothers = this.data.people.filter(
-            (person) => person.personId === grandmother.motherId
-          );
-          if (greatgrandmothers.length > 0) {
-            indent.class = "pl-6";
-          }
-        }
-      }
-    }
-
-    return indent;
-  }
-
-  @tracked isMother = false;
-
   get active() {
     return this.activePerson.personId === this.args.person.personId;
   }
 
   @action
   setActivePerson() {
-    this.activePerson.personId = this.args.person.personId;
+    this.activePerson.setActivePerson(this.args.person.personId);
   }
 }
