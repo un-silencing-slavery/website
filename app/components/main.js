7a16a14549f003ac7301796c2042f0907590d8d1
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-08-08 01:31:54 -0400

    feat: Hide modal on development always

--- app/components/main.js
+++ app/components/main.js
@@ -2,6 +2,7 @@ import Component from "@glimmer/component";
 import { action } from "@ember/object";
 import { service } from "@ember/service";
 import ModalsIndexComponent from "un-silencing-slavery/components/modals-index";
+import config from "un-silencing-slavery/config/environment";
 
 export default class MainComponent extends Component {
   @service modalIndex;
@@ -11,7 +12,7 @@ export default class MainComponent extends Component {
   @service data;
 
   @action showModal() {
-    if (this.modalIndex.showModal) {
+    if (this.modalIndex.showModal && config.environment === "production") {
       this.modalIndex.showModal = false;
       this.modals.open(ModalsIndexComponent);
     }
