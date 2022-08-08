82d9c2ecbba623ce3f8364162c12c6a1a4e94919
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-08-08 15:08:23 -0400

    feat: Implement headlessui menu for sort menu.

--- app/components/information/header.ts
+++ app/components/information/header.ts
@@ -1,13 +1,9 @@
 import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
-import { action } from "@ember/object";
 import { service } from "@ember/service";
 import DataService from "un-silencing-slavery/services/data";
 import RouterService from "@ember/routing/router-service";
 
 export default class InformationHeaderComponent extends Component {
-  @tracked sortmenuVisible = false;
-
   @service declare data: DataService;
 
   @service declare router: RouterService;
@@ -24,13 +20,5 @@ export default class InformationHeaderComponent extends Component {
 
   get route() {
     return this.router.currentRouteName;
-  }
-
-  @action toggleSortmenu() {
-    this.sortmenuVisible = !this.sortmenuVisible;
-  }
-
-  @action hideSortmenu() {
-    this.sortmenuVisible = false;
   }
 }
