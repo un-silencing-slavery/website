4bbf0b009a5ad9633127eb1201f884c30b9ab411
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-08-09 19:21:32 -0400

    chore(lint): lint

--- tests/integration/components/community-tree-container-test.js
+++ tests/integration/components/community-tree-container-test.js
@@ -3,10 +3,10 @@ import { setupRenderingTest } from "ember-qunit";
 import { render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 
-module("Integration | Component | community-tree/index", function(hooks) {
+module("Integration | Component | community-tree/index", function (hooks) {
   setupRenderingTest(hooks);
 
-  test.skip("it renders", async function(assert) {
+  test.skip("it renders", async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
